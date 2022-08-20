108897d7ea205b5fc8567be468f964ce1b988ac6
Devyanshi <devyanshi2106@gmail.com>
2022-08-21 02:20:09 +0530

    Added quiz.dartand result.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: unnecessary_new, avoid_print, prefer_const_constructors, deprecated_member_use, duplicate_ignore, sort_child_properties_last, unused_import
 
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 // void main() {
 //   runApp(MyHeheApp());
 // }
@@ -21,6 +21,21 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s my favourite Destination',
+      'answers': ['Newyork', 'Switzerland', 'Greece', 'Udaipur']
+    },
+    {
+      'questionText': 'What\'s my favourite Cuisine',
+      'answers': ['Thai', 'Indian', 'Chinese', 'Italian']
+    },
+    {
+      'questionText': 'What\'s my favourite food',
+      'answers': ['Pizza', 'Paani Puri', 'Dal makhni', 'Adi wle chawal']
+    },
+  ];
+
   var questionIndex = 0;
 
   void answerQuestion() {
@@ -29,41 +44,25 @@ class MyAppState extends State<MyApp> {
     });
 
     print(questionIndex);
+    if (questionIndex < questions.length) {
+      print('We have more questions');
+    }
   }
 
   @override //decorator provider by dart
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s my favourite Destination',
-        'answers': ['Newyork', 'Switzerland', 'Greece', 'Udaipur']
-      },
-      {
-        'questionText': 'What\'s my favourite Cuisine',
-        'answers': ['Thai', 'Indian', 'Chinese', 'Italian']
-      },
-      {
-        'questionText': 'What\'s my favourite food',
-        'answers': ['Pizza', 'Paani Puri', 'Dal makhni', 'Adi wle chawal']
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hehe App'),
-        ),
-        // ignore: prefer_const_literals_to_create_immutables
-        body: Column(children: [
-          Question(
-            questions[questionIndex > 2 ? questionIndex = 0 : questionIndex]
-                ['questionText'] as String,
+          appBar: AppBar(
+            title: const Text('Hehe App'),
           ),
-          ...(questions[questionIndex]['answers'] as List<String>)
-              .map((answer) {
-            return Answer(answerQuestion, answer);
-          }).toList()
-        ]),
-      ),
+          // ignore: prefer_const_literals_to_create_immutables
+          body: questionIndex < questions.length
+              ? Quiz(
+                  answerQuestion: answerQuestion,
+                  questions: questions,
+                  questionIndex: questionIndex)
+              : Result()),
     );
   }
 }
